107f36f28835d3027e5a69ec9caceb6675b4e502
vagrant <vagrant@localhost.localdomain>
2020-06-16 07:41:02 +0000

    URL added

---
+++ app/controllers/cart_products_controller.rb
@@ -0,0 +1,16 @@
+class CartProductsController < ApplicationController
+  def add
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def destroy_all
+  end
+
+  def update
+  end
+end
---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def confirmation
+  end
+
+  def complete
+  end
+
+  def create
+  end
+end
