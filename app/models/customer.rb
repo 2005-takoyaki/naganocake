b14f3e1de1e972635851f4993e35410d8c3d725e
vagrant <vagrant@localhost.localdomain>
2020-06-18 06:12:39 +0000

    [Finish]ユーザー登録・ログイン機能

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,12 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #新規登録時のバリデーション
+  validates :kana_last_name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :kana_first_name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+
 end
