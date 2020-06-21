1f71165b5f91f44e26b38cfb1c675fc6e793a180
vagrant <vagrant@localhost.localdomain>
2020-06-21 05:02:42 +0000

    [Finish]admin_order_show

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,5 @@ class Order < ApplicationRecord
   belongs_to :customer
 
   enum order_status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済: 4 },_suffix: true
+  enum payment_method: { クレジットカード: 0, 銀行振り込み: 1 },_suffix: true
 end
