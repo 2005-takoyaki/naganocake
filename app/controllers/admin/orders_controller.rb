55962b6c306c3c8a6e57f2dc92d5ddd437b933e0
vagrant <vagrant@localhost.localdomain>
2020-06-24 10:15:35 +0000

    [Update]注文ステータスと製作ステータスの更新にif文を追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -13,7 +13,7 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     if @order.update(order_params)
       if @order.order_status == "入金確認" # 注文ステータスが入金確認になれば、製作ステータスも製作待ちに変更
-        @order.order_products.update_all(production_status: 1)
+        @order.order_products.update_all(production_status: "製作待ち")
       end
       redirect_to admin_order_path(@order)
     else
@@ -23,7 +23,15 @@ class Admin::OrdersController < ApplicationController
 
   def products_update # 製作ステータスを更新する
     @order_product = OrderProduct.find(params[:id])
+    @order = @order_product.order
     if @order_product.update(order_product_params)
+      if @order_product.production_status == "製作中"# 製作ステータスが製作中になれば、注文ステータスも製作中に変更
+        @order.update(order_status: "製作中")
+      elsif @order_product.production_status == "製作完了"# 製作ステータスが全て製作完了になれば、注文ステータスも発送準備中に変更
+        if  @order.order_products.all? { |order_product| order_product.production_status == "製作完了" }
+          @order.update(order_status: "発送準備中")
+        end
+      end
       redirect_to admin_order_path(@order_product.order_id)
     else
       render 'show'
