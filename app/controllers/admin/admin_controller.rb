04554599cc8a5bfc7dc629688075bd733541b1ac
vagrant <vagrant@localhost.localdomain>
2020-06-24 03:31:43 +0000

    管理者ログイン後はログインページに飛べないように設定

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,7 +1,8 @@
 class Admin::AdminController < ApplicationController
   before_action :authenticate_admin, only: [:index, :destroy]
+  before_action :prevent_logged_in_admin, only: [:new, :create]
 
-  def new
+  def new # ログイン画面
   end
 
   def index
@@ -9,7 +10,7 @@ class Admin::AdminController < ApplicationController
     @today_order = Order.where(created_at: range)
   end
 
-  def create
+  def create # ログイン
     @admin = Admin.find_by(email: params[:admin][:email].downcase)
     if @admin && @admin.authenticate(params[:admin][:password])
       log_in @admin
@@ -23,9 +24,17 @@ class Admin::AdminController < ApplicationController
     end
   end
 
-  def destroy
+  def destroy # ログアウト
     log_out
     redirect_to admin_sign_in_path
+  end
+
+  private
+
+  def prevent_logged_in_admin
+    if current_admin
+      redirect_to admin_path
+    end
   end
 
 end
