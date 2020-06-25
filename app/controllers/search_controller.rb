50db5ee6642ad5972338951d3675d35a75167047
vagrant <vagrant@localhost.localdomain>
2020-06-25 02:28:16 +0000

    to

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,11 @@
 class SearchController < ApplicationController
   def index
+    content = params[:search][:content]
+    unless content.blank?
+      @products = Product.where('name LIKE ?', "%#{content}%")
+      @customers = Customer.where(['last_name LIKE ? OR first_name LIKE ?', "%#{content}%", "%#{content}%"])
+    else
+      redirect_to root_path
+    end
   end
 end
