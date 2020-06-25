5349b767c376ff0e99534ca63696289be3f6e0e6
vagrant <vagrant@localhost.localdomain>
2020-06-25 09:21:19 +0000

    finish

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,38 @@
 class ProductsController < ApplicationController
-	# before_action :authenticate_customer!only: [:index,:show]
+  # before_action :authenticate_customer!only: [:index,:show]
   def index
-  	@products = Product.page(params[:page]).per(8)
-  	@genres = Genre.all
-  	@products_cnt = Product.count
-  	@product_cnt = Genre.group(:product_id).count
+    @genres = Genre.where(genres: {is_valid: "true"})
+    # urlにgenre_id(params)がある場合
+    if params[:genre_id]
+      # Genreのデータベースのテーブルから一致するidを取得
+      genre = Genre.find(params[:genre_id])
+      # genre_idと紐づく商品を取得
+      products = genre.products.order(created_at: :desc)
+      @products = Kaminari.paginate_array(products).page(params[:page]).per(8)
+      @products_cnt = Product.count
+    else
+      # 投稿すべてを取得
+      @products_cnt = Product.count
+      products = Product.includes(:genre).where(genres: {is_valid: "true"})
+      @products = Kaminari.paginate_array(products).page(params[:page]).per(8)
+    end
+
   end
 
   def show
-    @genres = Genre.all
-  	@product = Product.find(params[:id])
+    @product = Product.find(params[:id])
     @cart_product = CartProduct.new
+    @genres = Genre.where(genres: {is_valid: "true"})
   end
 
   def top
+    @genres = Genre.where(genres: {is_valid: "true"})
+    @products = Product.page(params[:page]).per(4)
   end
 
   private
   def product_params
     params.require(:product).permit(:genre_id, :name, :introduction, :image, :non_taxed_price, :has_sold)
   end
+
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,13 @@
 module ProductsHelper
+
+def converting_to_jpy(price)
+    "#{price.to_s(:delimited, delimiter: ',')}"
+end
+
+def addTax(money,rate)
+   taxed_money =  (money*rate).round
+   "#{taxed_money.to_s(:delimited, delimiter: ',')}"
+
+end
+
 end
