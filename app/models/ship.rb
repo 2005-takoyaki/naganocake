fc9f233b4579c69e971c485578cc491aa5b67077
vagrant <vagrant@localhost.localdomain>
2020-06-27 07:51:05 +0000

    [Debug]郵便番号のバリデーションからpresence: trueを削除。コンクリフト分削除

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,7 +2,7 @@ class Ship < ApplicationRecord
   belongs_to :customer
 
   validates :name, presence: true
-  validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
+  validates :postal_code, format: { with: /\A\d{7}\z/ }
   validates :address, presence: true
 
   def view_all_customer_date
