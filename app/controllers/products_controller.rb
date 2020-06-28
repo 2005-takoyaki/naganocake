b57882d6833f58aa9e44dd78eddc9f882ad830d3
照屋翔大 <shota@mac.local>
2020-06-28 12:41:14 +0900

    finish

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,18 +2,20 @@ class ProductsController < ApplicationController
   
   def index
     @genres = Genre.where(genres: {is_valid: "true"})
+     products = Product.includes(:genre).where(genres: {is_valid: "true"}).where(has_sold: "true")
+    @products = Kaminari.paginate_array(products).page(params[:page]).per(8)
     # urlにgenre_id(params)がある場合
     if params[:genre_id]
       # Genreのデータベースのテーブルから一致するidを取得
       genre = Genre.find(params[:genre_id])
       # genre_idと紐づく商品を取得
-      products = genre.products.order(created_at: :desc)
+      products = genre.products.order(created_at: :desc).where(has_sold: "true")
       @products = Kaminari.paginate_array(products).page(params[:page]).per(8)
       @products_cnt = Product.count
     else
       # 投稿すべてを取得
       @products_cnt = Product.count
-      products = Product.includes(:genre).where(genres: {is_valid: "true"})
+      products = Product.includes(:genre).where(genres: {is_valid: "true"}).where(has_sold: "true")
       @products = Kaminari.paginate_array(products).page(params[:page]).per(8)
     end
 
@@ -27,7 +29,7 @@ class ProductsController < ApplicationController
 
   def top
     @genres = Genre.where(genres: {is_valid: "true"})
-    @products = Product.page(params[:page]).per(4)
+    @products = Product.page(params[:page]).per(4).where(has_sold: "true")
   end
 
   private
