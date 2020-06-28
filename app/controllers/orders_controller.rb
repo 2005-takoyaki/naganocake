1f8e2f261b60b9014be7a9116cd2be0310f5695b
vagrant <vagrant@localhost.localdomain>
2020-06-28 04:33:22 +0000

    さらにコメントを追加,さらに簡略化

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,8 @@ class OrdersController < ApplicationController
   before_action :authenticate_customer!
 
   def index
-    @orders = current_customer.orders
-    @order_products = current_customer.order_products
+    @orders = current_customer.orders # 自分の注文を取得
+    @order_products = current_customer.order_products # 自分の注文商品を取得
   end
 
   def show
@@ -18,11 +18,11 @@ class OrdersController < ApplicationController
   end
 
   def confirmation_new
-    @order = current_customer.orders.new(order_params)
-    if current_customer.ships.where(customer_id: current_customer.id).present?
-      @order_ship = Ship.find(params[:order][:ship][:id])
+    @order = current_customer.orders.new(order_params) # Orderを初期化
+    if current_customer.ships.where(customer_id: current_customer.id).present? # もし自分のShipテーブルが存在していたら
+      @order_ship = Ship.find(params[:order][:ship][:id]) # Shipテーブルのidを取得
     end
-    @order.fare = 800
+    @order.fare = 800 # 送料
     @ship = current_customer.ships.new(postal_code: params[:order][:ship][:postal_code], address: params[:order][:ship][:address], name: params[:order][:ship][:name])
     render :new unless @ship.valid? if params[:order][:key] == 'new_address'
     @cart_products = current_customer.cart_products
@@ -53,17 +53,17 @@ class OrdersController < ApplicationController
     taxed_total_price = total_price * 1.1
     order.billing_total = taxed_total_price.floor + 800
     order.order_status = 0
-
     order.save
-      cart_products.each do |cart_product|
-        order_products = order.order_products.new
-        order_products.product_id = cart_product.product_id
-        order_products.number = cart_product.quantity
-        order_products.taxed_price = cart_product.product.non_taxed_price * 1.1
-        order_products.production_status = 0
-        order_products.save
-        cart_product.destroy
-      end
+
+    cart_products.each do |cart_product|
+      order_products = order.order_products.new
+      order_products.product_id = cart_product.product_id
+      order_products.number = cart_product.quantity
+      order_products.taxed_price = cart_product.product.non_taxed_price * 1.1
+      order_products.production_status = 0
+      order_products.save
+      cart_product.destroy
+    end
 
     if params[:order][:key] == 'new_address'
       ship = current_customer.ships.new
@@ -82,9 +82,4 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:customer_id, :fare, :billing_total, :payment_method, :order_status, :postal_code, :address, :address_name)
   end
-
-  def ship_params
-    params.require(:order).permit(ship: [:customer_id, :name, :postal_code, :address])
-  end
-
 end
